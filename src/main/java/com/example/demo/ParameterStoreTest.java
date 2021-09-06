5a256feb937794a9bfcdf7863581552fed8d0647
bgr02 <snin023@naver.com>
2021-09-06 09:56:44 +0900

    add parameter store logic

---
+++ src/main/java/com/example/demo/ParameterStoreTest.java
@@ -0,0 +1,19 @@
+package com.example.demo;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ParameterStoreTest implements CommandLineRunner {
+
+    @Value("${profile.value}")
+    private String profile;
+
+    @Override
+    public void run(String... args) throws Exception {
+        System.out.println("Profile Property: " + profile);
+    }
+
+}
+
