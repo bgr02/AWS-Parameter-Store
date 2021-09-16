ef0d078f404bc29787612be60e9acbe5112e1f42
bgr02 <snin023@naver.com>
2021-09-16 21:14:40 +0900

    modify parameter print test logic

--- src/main/java/com/example/demo/ParameterStoreTest.java
+++ src/main/java/com/example/demo/ParameterStoreTest.java
@@ -9,10 +9,14 @@ public class ParameterStoreTest implements CommandLineRunner {
 
     @Value("${profile.value}")
     private String profile;
+	
+	@Value("${test.value}")
+    private String test_value;
 
     @Override
     public void run(String... args) throws Exception {
         System.out.println("Profile Property: " + profile);
+		System.out.println("Test Default Property: " + test_value);
     }
 
 }
